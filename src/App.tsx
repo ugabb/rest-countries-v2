2e509a1a74919ac195042f5786da2d108c7d797d
ugabb <gsb.alternativo@gmail.com>
2023-08-08 14:11:24 -0300

    filter function

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,44 @@ import CountryCard from "./components/CountryCard";
 
 function App() {
   const [countries, setCountries] = useState<any[]>([]);
+  const [filterCountries, setFilterCountries] = useState<any[]>([]);
+  const [countrySearched, setCountrySearched] = useState<string>("");
 
   useEffect(() => {
     getCountries();
   }, []);
+  useEffect(() => {
+    filterCountriesByPrompt();
+    // console.log(filterCountries);
+  }, [countrySearched]);
 
   const getCountries = async () => {
     try {
       const response = await fetch("https://restcountries.com/v3.1/all");
       const data = await response.json();
       setCountries(data);
-      console.log(countries);
+      console.log(countries[78].name.common);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const handleInputCountry = (e) => {
+    const { value } = e.target;
+    setCountrySearched(value);
+  };
+
+  const filterCountriesByPrompt = () => {
+    const filter = countries.filter((country) =>
+      country.name.common
+        .toLowerCase()
+        .startsWith(countrySearched.toLowerCase())
+    );
+    console.log("works filter", filter);
+    if (countrySearched == "") {
+      setFilterCountries(filter);
+    } else {
+      setFilterCountries([]);
     }
   };
 
@@ -26,20 +51,31 @@ function App() {
     <div className="">
       <Header theme={false} />
       <div className="flex flex-col md:flex-row justify-between">
-        <SearchBar />
+        <SearchBar handleInputCountry={handleInputCountry} />
         <FilterByRegion />
       </div>
       <main className="flex flex-col md:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {countries.map((country) => (
-          <CountryCard
-            key={country?.altSpellings[1]}
-            flag={country?.flags?.png}
-            name={country?.name?.common}
-            capital={country?.capital}
-            population={country?.population}
-            region={country?.region}
-          />
-        ))}
+        {filterCountries.length != 0
+          ? filterCountries.map((country) => (
+              <CountryCard
+                key={country?.altSpellings[1]}
+                flag={country?.flags?.png}
+                name={country?.name?.common}
+                capital={country?.capital}
+                population={country?.population}
+                region={country?.region}
+              />
+            ))
+          : countries.map((country) => (
+              <CountryCard
+                key={country?.altSpellings[1]}
+                flag={country?.flags?.png}
+                name={country?.name?.common}
+                capital={country?.capital}
+                population={country?.population}
+                region={country?.region}
+              />
+            ))}
       </main>
     </div>
   );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,9 +2,10 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 type Props = {
   // country: string;
+  handleInputCountry: () => void;
 };
 
-const SearchBar = (props: Props) => {
+const SearchBar = ({ handleInputCountry }: Props) => {
   return (
     <div className="flex items-center gap-3 bg-white p-3 my-5 mx-3 rounded-lg drop-shadow-custom md:w-1/2">
       <AiOutlineSearch className="text-3xl text-darkGray" />
@@ -12,6 +13,7 @@ const SearchBar = (props: Props) => {
         type="text"
         placeholder="Search for a country..."
         className="w-full p-2"
+        onChange={handleInputCountry}
       />
     </div>
   );
